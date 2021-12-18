7bf4d076d36cf1719340dbaf36e70f33f469bef7
krishnabharambe <krishnabharambe@gmail.com>
2021-12-19 01:48:58 +0530

    first commit

---
+++ api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path('index/',views.index),
+    path('ValidatePhone/',views.ValidatePhoneSendOTP.as_view()),
+    path('ValidateOTP/',views.ValidateOTP.as_view()),
+    path('register/',views.Register.as_view()),
+    path('login/',views.LoginAPI.as_view()),
+    path('userAPI/',views.UserAPI.as_view()),
+    path('changePassword/', views.ChangePasswordAPI.as_view()),
+    path('ValidatePhoneForgot/', views.ValidatePhoneForgot.as_view()),
+    path('ForgotValidateOTP/', views.ForgotValidateOTP.as_view()),
+    path('ForgetPasswordChange/', views.ForgetPasswordChange.as_view()),
+
+]
